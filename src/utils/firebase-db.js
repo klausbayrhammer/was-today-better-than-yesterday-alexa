6cea179b61e0684f0c0031f1d6172902ffae042f
Klaus Bayrhammer <klaus.bayrhammer@redbull.com>
2019-01-23 13:22:11 +0100

    makes firebase-credentials configurable via ENVs

--- src/utils/firebase-db.js
+++ src/utils/firebase-db.js
@@ -1,16 +1,27 @@
 const firebase = require('firebase/app');
 require('firebase/database');
 
+const DEFAULT_FIREBASE_CONFIG = {
+  apiKey: 'AIzaSyAC26X8bWaMmZJ-v5yr6NsJaLdXkOBDGIs',
+  authDomain: 'was-today-better-dev.firebaseapp.com',
+  databaseURL: 'https://was-today-better-dev.firebaseio.com',
+  projectId: 'was-today-better-dev',
+  storageBucket: 'was-today-better-dev.appspot.com',
+  messagingSenderId: '110009410201',
+};
+
+function getFirebaseConfig() {
+  if (process.env.FIREBASE_CONFIG) {
+    return JSON.parse(process.env.FIREBASE_CONFIG);
+  }
+  return DEFAULT_FIREBASE_CONFIG;
+}
+
 module.exports = new Promise(async (resolve) => {
-  firebase.initializeApp({
-    apiKey: 'AIzaSyAC26X8bWaMmZJ-v5yr6NsJaLdXkOBDGIs',
-    authDomain: 'was-today-better-dev.firebaseapp.com',
-    databaseURL: 'https://was-today-better-dev.firebaseio.com',
-    projectId: 'was-today-better-dev',
-    storageBucket: 'was-today-better-dev.appspot.com',
-    messagingSenderId: '110009410201',
-  });
-  await firebase.auth().signInWithEmailAndPassword('test-user@klausbayrhammer.com', '7gXAG2E5dxWTZsWzR9Q5');
+  firebase.initializeApp(getFirebaseConfig());
+  const email = process.env.FIREBASE_USER || 'test-user@klausbayrhammer.com';
+  const password = process.env.FIREBASE_PASSWORD || '7gXAG2E5dxWTZsWzR9Q5';
+  await firebase.auth().signInWithEmailAndPassword(email, password);
   const database = firebase.database();
   resolve(database);
 });
---
+++ test/utils/firebase-db.spec.js
@@ -0,0 +1,46 @@
+const expect = require('expect');
+const sinon = require('sinon');
+const proxyquire = require('proxyquire').noCallThru().noPreserveCache();
+
+describe('firebase-db.js', () => {
+  const signInWithEmailAndPassword = sinon.stub();
+  const initializeApp = sinon.stub();
+  const databaseStub = sinon.stub();
+  const firebase = { database: () => databaseStub, initializeApp, auth: () => ({ signInWithEmailAndPassword }) };
+
+  it('initializes firebase with default credentials', async () => {
+    const firebaseDb = proxyquire('../../src/utils/firebase-db',
+      { 'firebase/app': firebase, 'firebase/datbase': sinon.stub() });
+
+    const database = await firebaseDb;
+    expect(database).toEqual(databaseStub);
+    sinon.assert.calledWith(firebase.initializeApp, {
+      apiKey: 'AIzaSyAC26X8bWaMmZJ-v5yr6NsJaLdXkOBDGIs',
+      authDomain: 'was-today-better-dev.firebaseapp.com',
+      databaseURL: 'https://was-today-better-dev.firebaseio.com',
+      projectId: 'was-today-better-dev',
+      storageBucket: 'was-today-better-dev.appspot.com',
+      messagingSenderId: '110009410201',
+    });
+    sinon.assert.calledWith(signInWithEmailAndPassword, 'test-user@klausbayrhammer.com', '7gXAG2E5dxWTZsWzR9Q5');
+  });
+
+  it('initializes firebase with environment variable credentials', async () => {
+    process.env.FIREBASE_CONFIG = JSON.stringify({ firebase: 'config' });
+    const firebaseDb = proxyquire('../../src/utils/firebase-db',
+      { 'firebase/app': firebase, 'firebase/datbase': sinon.stub() });
+
+    await firebaseDb;
+    sinon.assert.calledWith(firebase.initializeApp, { firebase: 'config' });
+  });
+
+  it('uses env vars for user and password', async () => {
+    process.env.FIREBASE_USER = 'firebase-user';
+    process.env.FIREBASE_PASSWORD = 'firebase-password';
+    const firebaseDb = proxyquire('../../src/utils/firebase-db',
+      { 'firebase/app': firebase, 'firebase/datbase': sinon.stub() });
+
+    await firebaseDb;
+    sinon.assert.calledWith(signInWithEmailAndPassword, 'firebase-user', 'firebase-password');
+  });
+});
