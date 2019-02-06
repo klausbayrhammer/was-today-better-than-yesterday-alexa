78016ede83a781b30fd241401a2614dc7a90b1af
Klaus Bayrhammer <klaus.bayrhammer@redbull.com>
2019-02-06 09:32:41 +0100

    adds german translations

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,6 +6,7 @@ module.exports = {
   "rules": {
     "max-len": 0,
     "no-console" :0,
-    "no-param-reassign": 0
+    "no-param-reassign": 0,
+    "func-names": 0
   }
 };
--- src/translate/translation-keys.js
+++ src/translate/translation-keys.js
@@ -1,8 +1,14 @@
 module.exports = () => ({
   'en-GB': {
-    STREAK_AND_DONE_FOR_TODAY: 'Your current Streak for {0} is {1}, longest {2}. You are done for today - you added entries to all focus areas',
-    STREAK_AND_NEXT_FOCUS_AREA: 'Your current Streak for {0} is {1}, longest {2}. Was today better than yesterday in regards of {3}',
-    NEXT_FOCUS_AREA: 'Was today better than yesterday in regards of {0}',
-    DONE_FOR_TODAY: 'You are done for today - you added entries to all focus areas',
+    STREAK_AND_DONE_FOR_TODAY: 'Your current Streak for {0} is {1}, longest {2}. You are done for today - you added entries to all focus areas.',
+    STREAK_AND_NEXT_FOCUS_AREA: 'Your current Streak for {0} is {1}, longest {2}. Was today better than yesterday in regards of {3}.',
+    NEXT_FOCUS_AREA: 'Was today better than yesterday in regards of {0}.',
+    DONE_FOR_TODAY: 'You are done for today - you added entries to all focus areas.',
+  },
+  'de-DE': {
+    STREAK_AND_DONE_FOR_TODAY: 'Deine aktuelle Serie für {0} ist {1}, die längste ist {2}. Du bist fertig für heute.',
+    STREAK_AND_NEXT_FOCUS_AREA: 'Deine aktuelle Serie für {0} ist {1}, die längste ist {2}. War heute besser als gestern in Bezug auf {3}.',
+    NEXT_FOCUS_AREA: 'War heute besser als gestern in Bezug auf {0}.',
+    DONE_FOR_TODAY: 'Du bist fertig für heute.',
   },
 });
